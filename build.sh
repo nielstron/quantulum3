63a93a5df61fcd9411c4a47b38e37bf52caef233
Niels Mündler <n.muendler@tum.de>
2018-08-17 13:39:12 +0200

    Include introdoction on contributing in README

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # Script to run before pushing merged dev branch
 sed -i -e 's/https:\/\/travis-ci\.com\/nielstron\/quantulum3.svg?branch=dev/https:\/\/travis-ci\.com\/nielstron\/quantulum3.svg?branch=master/g' README.md
 pandoc README.md -o README.rst
---
+++ format.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Bash script to run before pushing changes to dev branch
+yapf -i -r .
