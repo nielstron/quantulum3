b8d19c8e7a0a28b7337d62424342e383e7cf8787
Niels Mündler <n.muendler@tum.de>
2018-08-17 14:00:00 +0200

    Include health badge

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 # Script to run before pushing merged dev branch
 sed -i -e 's/https:\/\/travis-ci\.com\/nielstron\/quantulum3.svg?branch=dev/https:\/\/travis-ci\.com\/nielstron\/quantulum3.svg?branch=master/g' README.md
+sed -i -e 's/https:\/\/landscape.io\/github\/nielstron\/quantulum3\/dev/https:\/\/landscape.io\/github\/nielstron\/quantulum3\/master/g' README.md
 pandoc README.md -o README.rst
