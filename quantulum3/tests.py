cf4c7ed800ad50b3cbb2c61b1b2dceb094062a33
stephengaw <stephen.gaw@teradata.com>
2018-09-05 09:21:26 +0100

    correct pep8

--- quantulum3/tests.py
+++ quantulum3/tests.py
@@ -157,7 +157,7 @@ class EndToEndTests(unittest.TestCase):
                     [quant.__dict__ for quant in quants],
                     [quant.__dict__ for quant in test['res']]))
 
-    @unittest.skip('Do not retrain classifiers, as currently overwrites clf.pickle and wiki.json files.')
+    @unittest.skip('Do not retrain classifiers, as overwrites clf.pickle and wiki.json files.')
     def test_training(self):
         # TODO - update test to not overwirte existing clf.pickle and wiki.json files.
         clf.train_classifier(False)
