0063d65622c8584794bf98d1cdea2bab7f21823d
Niels Mündler <n.muendler@tum.de>
2018-09-15 12:37:18 +0200

    New version

--- quantulum3/__init__.py
+++ quantulum3/__init__.py
@@ -1,10 +1,10 @@
 """quantulum3 init."""
 
-VERSION = (0, 3, 3)
+VERSION = (0, 3, 4)
 
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = 'Marco Lagi, nielstron, sohrabtowfighi, grhawk and Rodrigo Castro'
 __author_email__ = 'n.muendler@web.de'
-__copyright__ = 'Copyright (C) 2016 Marco Lagi, nielstron, sohrabtowfighi, grhawk and Rodrigo Castro'
+__copyright__ = 'Copyright (C) 2016 Marco Lagi, nielstron,t sohrabtowfighi, grhawk and Rodrigo Castro'
 __license__ = "MIT"
 __url__ = "https://github.com/nielstron/quantulum3"
