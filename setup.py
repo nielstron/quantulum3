f6d54edb19598689e5e30df4688091e4b0750b29
Niels Mündler <n.muendler@tum.de>
2018-09-24 20:52:34 +0200

    Add python version to setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX', 'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
