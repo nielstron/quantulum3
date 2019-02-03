2d4dfa347bb9eba2a04f0acfe5015b2e19bee6df
Niels Mündler <n.muendler@tum.de>
2019-02-03 23:07:09 +0100

    Fix double joining the load path for entities and units

--- quantulum3/load.py
+++ quantulum3/load.py
@@ -139,7 +139,7 @@ class Entities(object):
             for k in general_entities)
 
         # Update with language specific URI
-        with path.joinpath(language.topdir(lang), 'entities.json').open(
+        with TOPDIR.joinpath(language.topdir(lang), 'entities.json').open(
                 'r', encoding='utf-8') as file:
             lang_entities = json.load(file)
         for ent in lang_entities:
@@ -237,7 +237,7 @@ class Units(object):
         with path.open(encoding='utf-8') as file:
             general_units = json.load(file)
         # load language specifics
-        path = path.joinpath(language.topdir(lang), 'units.json')
+        path = TOPDIR.joinpath(language.topdir(lang), 'units.json')
         with path.open(encoding='utf-8') as file:
             lang_units = json.load(file)
 
