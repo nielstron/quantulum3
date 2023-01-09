366953be35b754e8bd29b68bf62593757b70a701
Niels Mündler <n.muendler@posteo.de>
2023-01-09 10:34:10 +0100

    Multilanguage property based tests

--- quantulum3/tests/test_hypothesis.py
+++ quantulum3/tests/test_hypothesis.py
@@ -6,36 +6,38 @@
 
 from hypothesis import settings, given, strategies as st
 
-from quantulum3.tests.test_setup import multilang
+from ..language import LANGUAGES
 from .. import parser as p
 
 import unittest
 
+multilang_strategy = st.one_of(*(st.just(l) for l in LANGUAGES))
+
 
 class TestNoErrors(unittest.TestCase):
-    @given(st.text())
+    @given(st.text(), multilang_strategy)
     @settings(deadline=None)
-    def test_parse(self, s):
+    def test_parse(self, s, lang):
         # Just assert that this does not throw any exceptions
-        p.parse(s)
+        p.parse(s, lang=lang)
 
-    @given(st.text())
+    @given(st.text(), multilang_strategy)
     @settings(deadline=None)
-    def test_extract_spellout_values(self, s):
+    def test_extract_spellout_values(self, s, lang):
         # Just assert that this does not throw any exceptions
-        p.extract_spellout_values(s)
+        p.extract_spellout_values(s, lang=lang)
 
-    @given(st.text())
+    @given(st.text(), multilang_strategy)
     @settings(deadline=None)
-    def test_inline_parse_and_expand(self, s):
+    def test_inline_parse_and_expand(self, s, lang):
         # Just assert that this does not throw any exceptions
-        p.inline_parse_and_expand(s)
+        p.inline_parse_and_expand(s, lang=lang)
 
-    @given(st.text())
+    @given(st.text(), multilang_strategy)
     @settings(deadline=None)
-    def test_clean_text(self, s):
+    def test_clean_text(self, s, lang):
         # Just assert that this does not throw any exceptions
-        p.clean_text(s)
+        p.clean_text(s, lang=lang)
 
 
 if __name__ == "__main__":
